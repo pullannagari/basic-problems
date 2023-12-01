86c18458369d82a0a0f4b0b0401ca749afc69b41
Pullannagari <sudhir.123reddy@hotmail.com>
2023-12-01 08:01:48 -0800

    adding more solutions

---
+++ dijkstras_shortest_path.py
@@ -0,0 +1,31 @@
+import heapq
+# dijkstra's algorithm to calculate shortest path
+# we can't use regular BFS to explore all the nodes level by level and find the shortest path since there are weights
+# we instead use dijkstra's which is kind of a BFS, but uses minheap instead of a queue to find the next node
+class Solution:
+    def shortestPath(self, n: int, edges: list, src: int) -> dict:
+        adj_list = {}
+        for i in range(n):
+            adj_list[i] = []
+        for edge in edges:
+            adj_list[edge[0]].append((edge[1], edge[2]))
+        shortest = {}
+        mheap = [(0,src)]
+        while mheap:
+            w1, n1 = heapq.heappop(mheap)
+            if n1 in shortest:
+                continue
+            shortest[n1] = w1
+            for n2, w2 in adj_list[n1]:
+                if n2 not in shortest:
+                    heapq.heappush(mheap, (w1+w2, n2))
+        # at the end, we should also add the nodes which are unreachable
+        for i in range(n):
+            if i not in shortest:
+                shortest[i] = -1
+        return shortest
+    
+if __name__ == "__main__":
+    sol = Solution()
+    edges = [[0,1,10], [0,2,3], [1,3,2], [2,1,4], [2,3,8], [2,4,2], [3,4,5]]
+    print(sol.shortestPath(5, edges, 0))
--- heap.py
+++ heap.py
@@ -1,76 +1,126 @@
-class Heap:
-    def __init__(self) -> None:
-        self.storage = []
-        
-    def _swap_and_adjust(self, dir):
-        if dir == "up":
-            index = len(self.storage)-1
-            while index > 0:
-                par_index = index // 2
-                if self.storage[par_index] > self.storage[index]:
-                    self.storage[par_index], self.storage[index] = self.storage[index], self.storage[par_index]
-                index = par_index
-        elif dir == "down":
-            index = 0
-            length = len(self.storage)-1
-            while index < len(self.storage):
-                left = index * 2 + 1
-                right = index * 2 + 2
-                if left <= length and right <= length:
-                    if self.storage[left] < self.storage[right]:
-                        if self.storage[left] < self.storage[index]:
-                            self.storage[index], self.storage[left] = self.storage[left], self.storage[index]
-                            index = left
-                        else:
-                            return
-                    else:
-                        if self.storage[right] < self.storage[index]:
-                            self.storage[index], self.storage[right] = self.storage[right], self.storage[index]
-                            index = right
-                        else:
-                            return
-                elif left <= length:
-                    if self.storage[left] < self.storage[index]:
-                        self.storage[index], self.storage[left] = self.storage[left], self.storage[index]
-                        index = left
-                    else:
-                        return
-                else:
-                    return
-                
+class MinHeap:
     
+    def __init__(self):
+        self.storage = []
+
     def push(self, val: int) -> None:
         self.storage.append(val)
-        if len(self.storage) == 1:
-            return
-        self._swap_and_adjust("up")
-            
-    
-    def peek(self) -> int:
+        curr_index = len(self.storage)-1
+        while curr_index > 0:
+            par_index = (curr_index -1) // 2
+            if self.storage[par_index] > self.storage[curr_index]:
+                self.storage[par_index], self.storage[curr_index] = self.storage[curr_index], self.storage[par_index]
+            curr_index = par_index
+
+    def down_propagation_helper(self, curr_index:int) -> None:
+        while curr_index < len(self.storage):
+            left = curr_index * 2 + 1
+            right = curr_index * 2 + 2
+            if left < len(self.storage) and right < len(self.storage):
+                if self.storage[left] < self.storage[right]:
+                    if self.storage[left] < self.storage[curr_index]:
+                        self.storage[left], self.storage[curr_index] = self.storage[curr_index], self.storage[left]
+                    curr_index = left
+                else:
+                    if self.storage[right] < self.storage[curr_index]:
+                        self.storage[right], self.storage[curr_index] = self.storage[curr_index], self.storage[right]
+                    curr_index = right
+            elif left < len(self.storage):
+                if self.storage[left] < self.storage[curr_index]:
+                    self.storage[left], self.storage[curr_index] = self.storage[curr_index], self.storage[left]
+                curr_index = left
+            else:
+                break
+
+    def pop(self) -> int:
+        if len(self.storage) > 1:
+            ret_val = self.storage[0]
+            repl_val = self.storage.pop()
+            self.storage[0] = repl_val
+            curr_index = 0
+            self.down_propagation_helper(curr_index)
+            return ret_val
+        elif len(self.storage) == 1:
+            return self.storage.pop()
+        else:
+            return -1
+
+    def top(self) -> int:
         if self.storage:
             return self.storage[0]
         return -1
-    
-    def pop(self) -> int:
-        if not self.storage:
-            return -1
-        val = self.storage[0]
-        self.storage[0] = self.storage[len(self.storage)-1]
-        self.storage.pop()
-        if not self.storage:
-            return val
-        self._swap_and_adjust("down")
-        return val
+        
+
+    def heapify(self, nums: list) -> None:
+        if nums:
+            self.storage = nums
+            curr_index = len(self.storage)//2
+            while curr_index >= 0:
+                self.down_propagation_helper(curr_index)
+                curr_index -= 1
+        
     
 if __name__ == "__main__":
-    hp = Heap()
-    hp.push(1)
-    hp.push(2)
-    hp.push(-1)
-    hp.push(-2)
-    hp.push(-10)
-    assert hp.pop() == -10
-    assert hp.pop() == -2
-    assert hp.pop() == -1
-    assert hp.pop() == 1
-    assert hp.pop() == 2
+    def tc1():
+        hp = MinHeap()
+        hp.push(1)
+        assert hp.top() == 1
+        assert hp.pop() == 1
+        assert hp.pop() == -1
+    def tc2():
+        hp = MinHeap()
+        hp.push(1)
+        hp.push(2)
+        assert hp.pop() == 1
+        assert hp.pop() == 2
+    def tc3():
+        hp = MinHeap()
+        hp.heapify([1,2,3,4,5])
+        assert hp.pop() == 1
+        assert hp.pop() == 2
+        assert hp.pop() == 3
+        assert hp.pop() == 4
+        assert hp.pop() == 5
+    def tc4():
+        hp = MinHeap()
+        hp.push(5)
+        hp.push(2)
+        hp.push(10)
+        assert hp.top() == 2
+        assert hp.pop() == 2
+        assert hp.top() == 5
+        assert hp.pop() == 5
+        assert hp.top() == 10
+        assert hp.pop() == 10
+        assert hp.top() == -1
+        assert hp.pop() == -1
+    def tc5():
+        hp = MinHeap()
+        hp.heapify([5, 4, 3, 2, 1])
+        assert hp.pop() == 1
+        assert hp.top() == 2
+    def tc6():
+        hp = MinHeap()
+        hp.heapify([3, 2, 1])
+        assert hp.pop() == 1
+        assert hp.top() == 2
+        assert hp.pop() == 2
+        assert hp.top() == 3
+    def tc7():
+        hp = MinHeap()
+        hp.push(10)
+        hp.heapify([5,3,7])
+        assert hp.pop() == 3
+        assert hp.top() == 5
+    def tc8():
+        hp = MinHeap()
+        hp.heapify([])
+        assert hp.pop() == -1
+    tc1()
+    tc2()
+    tc3()
+    tc4()
+    tc5()
+    tc6()
+    tc7()
+    tc8()

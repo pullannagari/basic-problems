a989caaca53718639d2124de949527c26496f941
Pullannagari <sudhir.123reddy@hotmail.com>
2023-12-01 09:09:00 -0800

    adding min spanning tree prims

---
+++ prims_min_spanning_tree.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumSpanningTree(self, n: int, edges: list) -> int:
+        adj_list = {}
+        for i in range(n):
+            adj_list[i] = []
+        for f, t, w in edges:
+            adj_list[f].append((t,w))
+            adj_list[t].append((f,w))
+        mst = []
+        visited = set()
+        mheap = [(0,0,0)]
+        total_weight = 0
+        while mheap:
+            edge = heapq.heappop(mheap)
+            if edge[2] not in visited:
+                visited.add(edge[2])
+                mst.append(edge)
+                total_weight += edge[0]
+                for n2, w in adj_list[edge[2]]:
+                    if n2 not in visited:
+                        heapq.heappush(mheap,(w, edge[2],n2))
+        for i in range(n):
+            if i not in visited:
+                return -1
+        return total_weight
